33c4f597a2b20b8e13a9489805b82ccb28781917
Joemike Grisola <167644452+maykmaykmayk@users.noreply.github.com>
2024-10-16 21:28:06 +0800

    Create Table

---
+++ schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE dreamjob_registration (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    gender VARCHAR(50),
+    age INT,
+    experience VARCHAR(50),
+    type_of_job VARCHAR(50)
+);
